4b94d87b41483b4354d610b1bef6773561e854a8
becherryu <81617058+becherryu@users.noreply.github.com>
2026-01-14 16:09:31 +0900

    feat: 고객 주문 가격 관련 constants

---
+++ JS/OrderSystem/utils/constants.js
@@ -0,0 +1,13 @@
+export const MEMBERSHIP_DC = {
+  VIP: 0.9,
+  Gold: 0.95,
+};
+
+export const PRICE_DC = {
+  over10: 5000, // 10만원 이상 할인
+  over20: 15000, // 20만원 이상 할인
+};
+
+export const SHIPPING_FEE = {
+  fast: 3000,
+};
