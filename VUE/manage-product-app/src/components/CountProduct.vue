ad1b919a82584495297081f420ac30d64e01a4fb
becherryu <81617058+becherryu@users.noreply.github.com>
2026-02-04 08:27:05 +0900

    feat: 상품 현황 컴포넌트

---
+++ VUE/manage-product-app/src/components/CountProduct.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  totalCount: Number,
+  totalPrice: Number,
+});
+</script>
+
+<template>
+  <div>
+    <h2>📊 상품 현황</h2>
+    <h3>상품 항목 개수 : {{ totalCount }}</h3>
+    <h3>상품 총 가격 : {{ totalPrice.toLocaleString() }} 원</h3>
+  </div>
+</template>
