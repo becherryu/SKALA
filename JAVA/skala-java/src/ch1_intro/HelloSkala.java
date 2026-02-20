51616935b17b67302a45d1be969a0b16e8dd5c7d
becherryu <81617058+becherryu@users.noreply.github.com>
2026-02-20 10:51:28 +0900

    update: helloSkala

--- JAVA/skala-java/src/ch1_intro/HelloSkala.java
+++ JAVA/skala-java/src/ch1_intro/HelloSkala.java
@@ -3,6 +3,6 @@ package ch1_intro;
 public class HelloSkala {
     // "스칼라에 오신 것을 환영합니다." 출력
     public static void main(String[] args) {
-        System.out.println("스칼라에 오신 것을 환영합니다.");  
+        System.out.println("스칼라에 오신 것을 환영합니다.");
     }
 }
