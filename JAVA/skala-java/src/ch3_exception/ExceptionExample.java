585052427689d384ec1a9de250a017167a0220ad
becherryu <81617058+becherryu@users.noreply.github.com>
2026-02-20 16:52:08 +0900

    feat: resources 자동 해제 실습

--- JAVA/skala-java/src/ch3_exception/ExceptionExample.java
+++ JAVA/skala-java/src/ch3_exception/ExceptionExample.java
@@ -8,24 +8,25 @@ public class ExceptionExample {
 
     // BufferedReader 사용하여 data.txt 파일의 내용을 읽어서 출력하는 메서드
     public static void main(String[] args) {
-        BufferedReader reader = null;
-        try {
-            reader = new BufferedReader(new FileReader("data.txt"));
+        // BufferedReader reader = null;
+        try (BufferedReader br = new BufferedReader(new FileReader("JAVA/skala-java/src/ch3_exception/data.txt"))) {
+            // reader = new BufferedReader(
+            // new FileReader("JAVA/skala-java/src/ch3_exception/data.txt"));
             String line;
-            while ((line = reader.readLine()) != null) {
+            while ((line = br.readLine()) != null) {
                 System.out.println(line);
             }
         } catch (IOException e) {
             System.err.println("예외가 발생했습니다: " + e.getMessage());
         } finally {
-            try {
-                if (reader != null) {
-                    System.out.println("리소스를 정리합니다.");
-                    reader.close();
-                }
-            } catch (IOException e) {
-                System.err.println("리소스 정리 중 예외가 발생했습니다: " + e.getMessage());
-            }
+            // try {
+            // if (reader != null) {
+            // System.out.println("리소스를 정리합니다.");
+            // reader.close();
+            // }
+            // } catch (IOException e) {
+            // System.err.println("리소스 정리 중 예외가 발생했습니다: " + e.getMessage());
+            // }
             System.out.println("파일 읽기 작업이 완료되었습니다.");
         }
     }
