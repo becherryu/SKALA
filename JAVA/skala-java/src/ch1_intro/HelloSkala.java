3d6b6e925c6af8370356d687ccf404ab03fd2935
becherryu <81617058+becherryu@users.noreply.github.com>
2026-02-20 10:24:34 +0900

    feat(HelloSkala): 환영 메시지 출력

--- JAVA/skala-java/src/ch1_intro/HelloSkala.java
+++ JAVA/skala-java/src/ch1_intro/HelloSkala.java
@@ -1,5 +1,8 @@
 package ch1_intro;
 
 public class HelloSkala {
-    // "스칼라에 오신 것을 환영합니다." 출력}
+    // "스칼라에 오신 것을 환영합니다." 출력
+    public static void main(String[] args) {
+        System.out.println("스칼라에 오신 것을 환영합니다.");  
+    }
 }
