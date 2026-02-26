a78c1ad6e6b7ac2a51401b2e7981062491d2afc8
becherryu <dkgus504@sookmyung.ac.kr>
2026-02-26 16:17:14 +0900

    feat(stock, player, playerStock): JPA 엔터티 생성

--- SPRINGBOOT/skala-stock-api/src/main/java/com/sk/skala/stockapi/data/table/Stock.java
+++ SPRINGBOOT/skala-stock-api/src/main/java/com/sk/skala/stockapi/data/table/Stock.java
@@ -1,13 +1,28 @@
 package com.sk.skala.stockapi.data.table;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Entity
+@Table(name = "stock")
+@Data
 public class Stock {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	private String stockName;
+	private Double stockPrice;
 
-	public Stock(String name, Double price) {
-		// implement codes here
+	protected Stock() {
+	}
+
+	public Stock(String stockName, Double stockPrice) {
+		this.stockName = stockName;
+		this.stockPrice = stockPrice;
 	}
 }
