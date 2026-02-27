5705ba9a0a550fa30abd26e24e1b77d3de6ce777
becherryu <dkgus504@sookmyung.ac.kr>
2026-02-27 09:46:11 +0900

    feat: playerStock DTO 생성

--- SPRINGBOOT/skala-stock-api/src/main/java/com/sk/skala/stockapi/data/dto/PlayerStockDto.java
+++ SPRINGBOOT/skala-stock-api/src/main/java/com/sk/skala/stockapi/data/dto/PlayerStockDto.java
@@ -1,4 +1,19 @@
 package com.sk.skala.stockapi.data.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PlayerStockDto {
+    private Long stockId;
+    private String stockName;
+    private Double stockPrice;
+    private Integer quantity;
 }
